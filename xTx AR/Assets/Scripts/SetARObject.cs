8bac220ee16fbb6db03bc020f2dca89af7ae8dc7
Enrique Molina <enrique@2020xr.com>
2020-01-08 19:36:32 -0800

    Can place objects!

---
+++ xTx AR/Assets/Scripts/SetARObject.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetARObject : MonoBehaviour
+{
+    public ARController controller;
+    public GameObject arObject;
+
+    public void ChangeCurrentARObject()
+    {
+        controller.SetGameObject(arObject);
+    }
+}
